c8bd06f96f005adf9633f0056eb2743e826f94d4
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2019-09-19 01:33:00 -0400

    enable and secure api keys

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const passport = require('passport');
+const keys = require('./config/keys');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send({
-    hi: 'There',
-  });
-});
+app.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    accessToken => {
+      console.log(accessToken);
+    },
+  ),
+);
 
 const PORT = process.env.PORT || 5000;
 
